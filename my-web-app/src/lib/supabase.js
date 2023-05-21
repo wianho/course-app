80d84be1bbe21eb1168dd741870eb51f49866def
andy <andy@example.com>
2023-05-21 10:52:24 -0400

    Getting alot done!

--- my-web-app/src/lib/supabase.js
+++ my-web-app/src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL; // Replace with your Supabase URL
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY; // Replace with your Supabase public API key
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY; // Replace with your Supabase public API key
 
 const supabase = createClient (supabaseUrl, supabaseKey);
 
